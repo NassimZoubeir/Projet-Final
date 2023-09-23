248ef135627e71eb253248b34449983d597caf23
Nassim <nassimz@outlook.fr>
2023-09-23 19:19:21 +0200

    correction detail.js

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -61,7 +61,7 @@ fetchDataFromServer(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${api
         casts: { cast, crew },
         videos: { results: videos }
     } = movie;
-
+    
     // Modification du titre de la page
     document.title = `${title} - Nassflix`;
 
@@ -71,19 +71,74 @@ fetchDataFromServer(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${api
 
     // Remplissage de l'élément avec les données du film
     movieDetail.innerHTML = `
-        <div class="backdrop-image" style="background-image: url(${imageBaseUrl}w1280${backdrop_path || poster_path});"></div>
-        <figure class="poster-box movie-poster">
-            <img src="${imageBaseUrl}w342${poster_path}" alt="${title} poster" class="img-cover">
-        </figure>
-        <div class="detail-box">
-            <!-- ... (autres détails du film) ... -->
+    <div class="backdrop-image" style="background-image: url(${imageBaseUrl}w1280${backdrop_path || poster_path});"></div>
+
+    <figure class="poster-box movie-poster">
+        <img src="${imageBaseUrl}w342${poster_path}" alt="${title} poster" class="img-cover">
+    </figure>
+
+    <div class="detail-box">
+
+        <div class="detail-content">
+            <h1 class="heading">${title}</h1>
+            
+            <div class="meta-list">
+
+                <div class="meta-item">
+                    <img src="./assets/images/star.png" width="20" height="20" alt="rating">
+                    <span class="span">${vote_average.toFixed(1)}</span>
+                </div>
+
+                <div class="separator"></div>
+
+                <div class="meta-item">${runtime}m</div>
+
+                <div class="separator"></div>
+
+                <div class="meta-item">${release_date.split("-")[0]}</div>
+
+                <div class="meta-item card-badge">${certification}</div>
+
+            </div>
+
+            <p class="genre">${getGenres(genres)}</p>
+
+            <p class="overview">${overview}</p>
+
+            <ul class="detail-list">
+
+                <div class="list-item">
+                    <p class="list-name">Avec</p>
+
+                    <p>${getCasts(cast)}</p>
+                </div>
+
+                <div class="list-item">
+                    <p class="list-name">Réalisé par</p>
+
+                    <p>${getDirectors(crew)}</p>
+                </div>
+
+            </ul>
         </div>
+
+        <div class="title-wrapper">
+            <h3 class="title-large">Bandes-annonces et Extraits</h3>
+        </div>
+
+        <div class="slider-list">
+            <div class="slider-inner"></div>
+        </div>
+
+    </div>
     `;
 
     // Ajout des vidéos (trailers et teasers)
     for (const { key, name } of filterVideos(videos)) {
+
         const videoCard = document.createElement("div");
         videoCard.classList.add("video-card");
+        
         videoCard.innerHTML = `
             <iframe width="500" height="294" src="https://www.youtube.com/embed/${key}?&theme=dark&color=white&rel=0" frameborder="0" allowfullscreen="1" title="${name}" class="img-cover" loading="lazy"></iframe>
         `;
